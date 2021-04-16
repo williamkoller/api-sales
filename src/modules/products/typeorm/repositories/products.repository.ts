7a14390f02d7ec9129b45c9311bfbdd1be9e241b
william koller <wkoller25@gmail.com>
2021-04-16 00:09:09 -0300

    feat: add ProductRepository in repositories

---
+++ src/modules/products/typeorm/repositories/products.repository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Product from '@modules/products/typeorm/entities/Product';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    return await this.findOne({ where: { name } });
+  }
+}
