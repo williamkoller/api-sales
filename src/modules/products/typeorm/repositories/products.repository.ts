a8713ca796e0a0fe13efd8b9400ca1bebcdcab16
william koller <wkoller25@gmail.com>
2021-04-16 10:57:53 -0300

    fix: resolve path absolute

--- src/modules/products/typeorm/repositories/products.repository.ts
+++ src/modules/products/typeorm/repositories/products.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import Product from '@modules/products/typeorm/entities/Product';
+import Product from '@modules/products/typeorm/entities/product';
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
