ca350926fd6d57a1004c0e41695331e502295a61
william koller <wkoller25@gmail.com>
2021-04-15 22:48:25 -0300

    chore: call reflect-metadata and config typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,13 @@
+import 'reflect-metadata';
+require('dotenv').config();
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
-
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
@@ -23,6 +26,6 @@ app.use(
   },
 );
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
 });
