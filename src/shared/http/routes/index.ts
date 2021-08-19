668a7530286e2a8ff33a2d032879c151e262c15c
william koller <wkoller25@gmail.com>
2021-08-19 01:30:24 -0300

    chore: add dotenv and config

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 import productsRouter from '@modules/products/routes/products.routes';
+import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
