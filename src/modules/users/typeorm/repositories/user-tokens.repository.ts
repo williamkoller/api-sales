ee0f0365b9231d8466da7c91722c621f4d60123d
William Koller <williamkoller404@gmail.com>
2022-02-26 11:29:22 -0300

    feat: add UserTokensRepository

---
+++ src/modules/users/typeorm/repositories/user-tokens.repository.ts
@@ -0,0 +1,22 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { UserToken } from '@modules/users/typeorm/entities/user-token';
+
+@EntityRepository(UserToken)
+export class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return await this.findOne({
+      where: { token },
+    });
+  }
+
+  public async findByUserId(userId: string): Promise<UserToken | undefined> {
+    return await this.findOne({
+      where: { userId },
+    });
+  }
+
+  public async generateToken(userId: string): Promise<UserToken | undefined> {
+    const userToken = this.create({ userId });
+    return await this.save(userToken);
+  }
+}
