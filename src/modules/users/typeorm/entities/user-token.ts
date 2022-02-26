ef3d59efbd2c6b2c0ac0cceb14ae50a3cdfc5635
William Koller <williamkoller404@gmail.com>
2022-02-26 11:18:09 -0300

    feat: add entity user_token

---
+++ src/modules/users/typeorm/entities/user-token.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user_tokens')
+export class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
