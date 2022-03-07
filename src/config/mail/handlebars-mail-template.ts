390261d2ed99a3fb0ee785c3df5ac0662465f770
William Koller <williamkoller404@gmail.com>
2022-03-07 20:06:18 -0300

    refactor: change to read file from hbs with file system

--- src/config/mail/handlebars-mail-template.ts
+++ src/config/mail/handlebars-mail-template.ts
@@ -1,20 +1,21 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 export interface ITemplateVariable {
   [key: string]: string | number;
 }
 
 export interface IParseMailTemplate {
-  template: string;
+  file: string;
   variables: ITemplateVariable;
 }
 
 export default class HandlebarsMailTemplate {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+  public async parse({ file, variables }: IParseMailTemplate): Promise<string> {
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
