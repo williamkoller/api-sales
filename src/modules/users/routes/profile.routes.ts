67086e89f189434db509c5fed0659161d5dd1db0
William Koller <williamkoller404@gmail.com>
2022-03-07 21:52:51 -0300

    feat: add profileRouter

---
+++ src/modules/users/routes/profile.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import isAuthenticated from '@shared/http/middlewares/is-authenticated';
+import ProfileController from '../controllers/profile.controller';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(isAuthenticated);
+
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().optional(),
+      oldPassword: Joi.string(),
+      passwordConfirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileController.update,
+);
+
+export default profileRouter;
