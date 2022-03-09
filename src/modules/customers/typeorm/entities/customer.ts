0db7938ba43c58fce77e6fbb4a8b6d1bc51afaf7
William Koller <williamkoller404@gmail.com>
2022-03-08 23:04:32 -0300

    feat: add entity customer

---
+++ src/modules/customers/typeorm/entities/customer.ts
@@ -0,0 +1,33 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('customers')
+class Customer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @BeforeInsert()
+  emailToLowerCase(): void {
+    this.email = this.email.toLowerCase();
+  }
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Customer;
