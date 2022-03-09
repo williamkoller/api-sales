822ad40920210aa683e556402144c32ff150947a
William Koller <williamkoller404@gmail.com>
2022-03-09 13:18:52 -0300

    refactor: alter logic for returns email

--- src/modules/customers/services/update-customer.service.ts
+++ src/modules/customers/services/update-customer.service.ts
@@ -1,10 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { StatusCodes } from 'http-status-codes';
 import { getCustomRepository } from 'typeorm';
-import User from '@modules/users/typeorm/entities/user';
-import { UsersRepository } from '@modules/users/typeorm/repositories/users.repository';
-import { compare } from 'bcrypt';
-import { hashPassword } from 'src/utils/hash-password';
 import { CustomersRepository } from '../typeorm/repositories/customers.repository';
 import Customer from '../typeorm/entities/customer';
 
@@ -16,6 +12,7 @@ interface IRequest {
 
 export class UpdateCustomerService {
   public async execute({ id, name, email }: IRequest): Promise<Customer> {
+    let customerData: Customer | undefined;
     const customersRepository = getCustomRepository(CustomersRepository);
     const customer = await customersRepository.findById(id);
 
@@ -23,10 +20,13 @@ export class UpdateCustomerService {
       throw new AppError('Customer not found.', StatusCodes.NOT_FOUND);
     }
 
-    const customerEmail = await customersRepository.findByEmail(email);
+    customerData = await customersRepository.findByEmail(email);
 
-    if (customerEmail && email !== customerEmail.email) {
-      throw new AppError('There is already on customer with this email.');
+    if (customerData && email !== customer.email) {
+      throw new AppError(
+        'There is already on customer with this email.',
+        StatusCodes.CONFLICT,
+      );
     }
 
     customer.name = name;
