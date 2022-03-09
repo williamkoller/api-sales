5876b92d69bb21921a29c7edfeb1dd86bc978886
William Koller <williamkoller404@gmail.com>
2022-03-08 23:08:27 -0300

    feat: add repository customer with methods findByName, findById and findByEmail

--- src/modules/customers/typeorm/repositories/customers.repository.ts
+++ src/modules/customers/typeorm/repositories/customers.repository.ts
@@ -2,4 +2,16 @@ import { EntityRepository, Repository } from 'typeorm';
 import Customer from '@modules/customers/typeorm/entities/customer';
 
 @EntityRepository(Customer)
-export class CustomersRepository extends Repository<Customer> {}
+export class CustomersRepository extends Repository<Customer> {
+  public async findByName(name: string): Promise<Customer | undefined> {
+    return await this.findOne({ where: { name } });
+  }
+
+  public async findById(id: string): Promise<Customer | undefined> {
+    return await this.findOne({ where: { id } });
+  }
+
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    return await this.findOne({ where: { email } });
+  }
+}
