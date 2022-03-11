49db5b6721ca6754225351e00f4add4ffbe6ec66
William Koller <williamkoller404@gmail.com>
2022-03-10 23:10:32 -0300

    feat: add entity order

--- src/modules/orders/typeorm/entities/order.ts
+++ src/modules/orders/typeorm/entities/order.ts
@@ -1,1 +1,22 @@
-export class Order {}
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Order;
